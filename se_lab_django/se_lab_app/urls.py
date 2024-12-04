008d1b0d50e70d4b7a7027c9d793643bf24aaccf
Patrick Carvalho <patrickcarvalho448@gmail.com>
2024-12-04 10:44:34 +0100

    JWT Auth

--- se_lab_django/se_lab_app/urls.py
+++ se_lab_django/se_lab_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet, CustomerViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -8,6 +9,10 @@ router.register(r'orders', OrderViewSet, basename='order')
 router.register(r'customers', CustomerViewSet, basename='customer')
 
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(),
+    name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(),
+    name='token_refresh')
 ]
 
--- se_lab_django/se_lab_app/views.py
+++ se_lab_django/se_lab_app/views.py
@@ -1,16 +1,20 @@
 from .models import Product, Order, Customer
 from rest_framework import viewsets
 from .serializers import ProductSerializer, OrderSerializer, CustomerSerializer
-
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminOrReadOnly
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
