6af5d56406177a6869d61e8f894032463973cc52
Patrick Carvalho <patrickcarvalho448@gmail.com>
2025-01-08 12:46:19 +0100

    Lab 9 - Part 3: update views.py

--- se_lab_django/se_lab_app/views.py
+++ se_lab_django/se_lab_app/views.py
@@ -3,11 +3,14 @@ from rest_framework import viewsets
 from .serializers import ProductSerializer, OrderSerializer, CustomerSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly
+from rest_framework.filters import SearchFilter
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    filter_backends = (SearchFilter,)
+    search_fields = ['name']
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
