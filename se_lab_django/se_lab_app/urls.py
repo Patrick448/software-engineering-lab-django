428eac141529e08e44c17ce58d9b87519802ad2e
Patrick Carvalho <patrickcarvalho448@gmail.com>
2025-01-08 09:47:46 +0100

    Lab 9 - Part 1 : Adds Swagger API

--- se_lab_django/se_lab_app/urls.py
+++ se_lab_django/se_lab_app/urls.py
@@ -2,6 +2,20 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet, CustomerViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Software engineering lab",
+        default_version="v1",
+        description="API documentation for the lab",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    authentication_classes=[],
+)
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -11,8 +25,9 @@ router.register(r'customers', CustomerViewSet, basename='customer')
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(),
-    name='token_obtain_pair'),
+         name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),
-    name='token_refresh')
+         name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger',
+        cache_timeout=0), name='schema-swagger-ui')
 ]
-
--- se_lab_django/se_lab_django/settings.py
+++ se_lab_django/se_lab_django/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'se_lab_app'
+    'se_lab_app',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
